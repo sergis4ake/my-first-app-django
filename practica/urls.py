4cabb195cc69770229be679a12dfecaa378ec42e
Sergiu Ghior <sergiu_svd@hotmail.com>
2018-03-08 19:52:31 +0100

    Añadidos templates y estructura MVC

--- practica/urls.py
+++ practica/urls.py
@@ -3,4 +3,7 @@ from practica import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'alumnos/', views.alumnos, name='alumnos'),
+    url(r'actividades/', views.actividades, name='actividades'),
+    url(r'informes/', views.informes_actividades, name='informes_actividades'),
 ]
--- practica/views.py
+++ practica/views.py
@@ -1,13 +1,43 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
-from django.http import HttpResponse
+from django.template import loader
+
 from practica.models import Alumno, Actividad, InformeActividad
 
-def index2(request):
-    usuarios = Usuario.objects.all()
-    respuesta = "Usuarios <br/>"
-    respuesta += '<br/>'.join(["nombre: %s, edad: %s"%(u.nombre, u.edad) for u in usuarios])
-    return HttpResponse(respuesta)
-
 def index(request):
-    return render_to_response('practica/index.html')
+    template = loader.get_template('practica/index.html')
+    title = "Menú Principal"
+    context = {
+        'title' : title,
+    }
+    return HttpResponse(template.render(context, request))
+
+def alumnos(request):
+    alumnos = Alumno.objects.all()
+    template = loader.get_template('practica/alumnos.html')
+    title = "Alumnos"
+    context = {
+        'title' : title,
+        'alumnos' : alumnos,
+    }
+    return HttpResponse(template.render(context, request))
+
+def actividades(request):
+    actividades = Actividad.objects.all()
+    template = loader.get_template('practica/actividades.html')
+    title = "Actividades"
+    context = {
+        'title' : title,
+        'actividades' : actividades,
+    }
+    return HttpResponse(template.render(context, request))
+
+def informes_actividades(request):
+    informes_actividades = InformeActividad.objects.all()
+    template = loader.get_template('practica/informes.html')
+    title = "Informes"
+    context = {
+        'title' : title,
+        'informes_actividades' : informes_actividades,
+    }
+    return HttpResponse(template.render(context, request))
